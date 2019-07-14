73ca75bb308c1f31f765c491f1d91143ed8d92cf
Chris Burke <burke1791@gmail.com>
2019-07-14 00:31:21 -0500

    removed shards and installed ant design

--- client/src/components/authModal/authModal.js
+++ client/src/components/authModal/authModal.js
@@ -1,14 +1,5 @@
 import React, { useState } from 'react';
 
-import {
-  Modal,
-  ModalBody,
-  ModalHeader,
-  Form,
-  FormGroup,
-  FormInput,
-  Button
-} from 'shards-react';
 import { AUTH_MODAL_TYPE } from '../../utilities/constants';
 
 function AuthModal(props) {
@@ -20,61 +11,10 @@ function AuthModal(props) {
     type: String - either 'signin' or 'signup'
   } */
 
-  const [emailVal, setEmailVal] = useState('');
-
-  const handleEmailChange = (event) => {
-    console.log(event.target.value);
-    setEmailVal(event.target.value);
-  }
-
-  const generateForm = () => {
-    if (props.type === AUTH_MODAL_TYPE.signin) {
-      return (
-        <Form>
-          <FormGroup>
-            <label htmlFor='#email'>Email Address</label>
-            <FormInput type='email' id='#email' placeholder='Email' value={emailVal} onChange={handleEmailChange} />
-          </FormGroup>
-          <FormGroup>
-            <label htmlFor='#password'>Password</label>
-            <FormInput type='password' id='#password' placeholder='Password' />
-          </FormGroup>
-        </Form>
-      );
-    } else if (props.type === AUTH_MODAL_TYPE.signup) {
-      return (
-        <Form>
-          <FormGroup>
-            <label htmlFor='#username'>Username</label>
-            <FormInput type='text' id='#username' placeholder='Username' />
-          </FormGroup>
-          <FormGroup>
-            <label htmlFor='#email'>Email Address</label>
-            <FormInput type='email' id='#email' placeholder='Email' />
-          </FormGroup>
-          <FormGroup>
-            <label htmlFor='#password'>Password</label>
-            <FormInput type='password' id='#password' placeholder='Password' />
-          </FormGroup>
-          <FormGroup>
-            <label htmlFor='#confirmPassword'>Confirm Password</label>
-            <FormInput type='password' id='#confirmPassword' placeholder='Password' />
-          </FormGroup>
-        </Form>
-      );
-    }
-  }
+  
 
   return (
-    <Modal open={props.open} toggle={props.toggle}>
-      <ModalHeader toggle={props.close}>
-        {props.label}
-      </ModalHeader>
-      <ModalBody>
-        {generateForm()}
-        <Button theme='primary'>Submit</Button>
-      </ModalBody>
-    </Modal>
+    <div></div>
   );
 }
 
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -2,79 +2,15 @@ import React, { useState } from 'react';
 
 import AuthModal from '../authModal/authModal';
 
-import {
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Collapse,
-  Button
-} from 'shards-react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { AUTH_MODAL_TYPE } from '../../utilities/constants';
 
 function Header() {
 
-  const [collapseOpen, setCollapseOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [authModalOpen, setAuthModalOpen] = useState(false);
-  const [authModalLabel, setAuthModalLabel] = useState('Sign In');
-  const [authModalType, setAuthModalType] = useState(AUTH_MODAL_TYPE.signin);
-
-  const toggleNavbar = () => {
-    setCollapseOpen(!collapseOpen);
-  }
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  }
-
-  const toggleAuthModal = (type) => {
-    if (type === AUTH_MODAL_TYPE.signin) {
-      setAuthModalLabel('Please Sign In');
-    } else if (type === AUTH_MODAL_TYPE.signup) {
-      setAuthModalLabel('Create an Account');
-    } else {
-      console.log('invalid auth modal type');
-    }
-    setAuthModalOpen(!authModalOpen);
-    setAuthModalType(type);
-  }
-
-  const closeAuthModal = () => {
-    setAuthModalOpen(false);
-  }
+  
 
   return (
-    <Navbar type='dark' theme='primary' expand='sm'>
-      <NavbarBrand href='#'>Calcutta</NavbarBrand>
-      <NavbarToggler onClick={toggleNavbar} />
-
-      <Collapse open={collapseOpen} navbar>
-        <Nav navbar  className='ml-auto'>
-          <Dropdown open={dropdownOpen} toggle={toggleDropdown}>
-            <DropdownToggle nav caret>
-              My Account
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem className='text-center'>
-                <a className='btn btn-primary' href='#' onClick={() => toggleAuthModal(AUTH_MODAL_TYPE.signin)}>Sign In</a>
-              </DropdownItem>
-              <DropdownItem className='text-center'>
-                <a className='btn btn-link' href='#' onClick={() => toggleAuthModal(AUTH_MODAL_TYPE.signup)}>Create an Account</a>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </Nav>
-      </Collapse>
-      <AuthModal open={authModalOpen} toggle={toggleAuthModal} close={closeAuthModal} label={authModalLabel} type={authModalType} />
-    </Navbar>
-  )
+    <div></div>
+  );
 }
 
 export default Header;
