ae032721a9689023cbc72736714325fa770c809c
Chris Burke <burke1791@gmail.com>
2019-07-21 14:14:39 -0500

    added a todo message for error handling

--- config/orm.js
+++ config/orm.js
@@ -4,6 +4,7 @@ let orm = {
   insert: (params, cb) => {
     let queryString = 'INSERT INTO ?? SET ?';
     let query = connection.query(queryString, [params.table, params.data], (err, result) => {
+      // @TODO error handling!!
       cb(err, result);
     });
     if (params.debug) {
