b0e71cc1310d1e584f789e796938279ce89899d0
Chris Burke <burke1791@gmail.com>
2019-07-21 16:29:45 -0500

    added a deeply nested database insert function for league creation

--- controllers/leagues.js
+++ controllers/leagues.js
@@ -1,4 +1,6 @@
 const league = require('../models/league.js');
+const firebase = require('../models/firebase');
+
 
 module.exports = function(app) {
   // =============== GET ROUTES ===============
@@ -6,6 +8,12 @@ module.exports = function(app) {
 
   // =============== POST ROUTES ==============
   
+  // this route does quite a bit:
+  // creates auction node in firebase ->
+  // inserts record in the leagues table (needs firebase guid) ->
+  // inserts record in the league_membership table (needs new league_id) ->
+  // inserts record in the league_settings table (needs new league_id) ->
+  // populates the league_teams table with records of each team in the current tournament
   app.post('/api/create_league', (req, res, next) => {
     let params = {
       league_name: req.body.name,
@@ -13,15 +21,60 @@ module.exports = function(app) {
       league_year: req.body.year,
       league_status: 'pre-auction'
     };
-    league.create(params, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.status(200).json({
-          league_id: result.insertId
-        });
-      }
+
+    // call firebase to create the league and auction nodes before inserting into MySQL
+    firebase.createAuctionNode().then(auctionId => {
+      params.auction_id = auctionId;
+      console.log(params);
+
+      league.create(params, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          let leagueMembership = {
+            user_id: req.body.user_id,
+            league_id: result.insertId,
+            role: 'creator'
+          };
+          // insert a league membership record
+          league.createMembership(leagueMembership, (err, result) => {
+            if (err) {
+              console.log(err);
+            } else {
+              // create league settings table
+              // all fields except the league_id are defaulted
+              let settingsObj = {
+                league_id: leagueMembership.league_id
+              };
+              league.createSettings(settingsObj, (err, result) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  // populate the league_teams table with the teams in the current year's tournament
+                  // @TODO do this conditionally based on whether or not this year's teams have been inserted in the first place (after selection sunday)
+                  league.populateTeams({ league_id: leagueMembership.league_id, year: 2019 }, (err, result) => {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      // finally done with this ridiculous cascade of queries
+                      res.status(200).json({
+                        message: 'league created'
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }).catch(error => {
+      console.log(error);
     });
+
+    /*
+    
+    */
   });
 }
 
