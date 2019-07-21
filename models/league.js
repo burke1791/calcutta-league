41beca8c188fa01e66b3eaef43e92f226f0651d6
Chris Burke <burke1791@gmail.com>
2019-07-21 16:31:57 -0500

    added three functions to populate all the necessary tables in MySQL for new leagues

--- models/league.js
+++ models/league.js
@@ -1,12 +1,46 @@
 const orm = require('../config/orm');
 
 let league = {
+  // cascading create queries to the following tables:
+  // leagues
+  // league_membership
+  // league_settings
+  // league_teams
   create: (leagueObj, cb) => {
     let queryParams = {
       table: 'leagues',
       data: leagueObj // keys must match column names
     };
     orm.insert(queryParams, cb);
+  },
+
+  createMembership: (membershipObj, cb) => {
+    let queryParams = {
+      table: 'league_membership',
+      data: membershipObj
+    };
+    orm.insert(queryParams, cb);
+  },
+
+  createSettings: (settingsObj, cb) => {
+    let queryParams = {
+      table: 'league_settings',
+      data: settingsObj
+    };
+    orm.insert(queryParams, cb);
+  },
+
+  populateTeams: (teamsObj, cb) => {
+    let queryParams = {
+      insertTable: 'league_teams',
+      columns: '(league_id, team_id)',
+      select: ['leagues.league_id', 'tournament_seeds.team_id'],
+      fromTable: 'tournament_seeds',
+      join: 'leagues',
+      where: { 'leagues.league_id': teamsObj.league_id },
+      and: { 'tournament_seeds.year': teamsObj.year }
+    };
+    orm.insertSelectJoinWhereAnd(queryParams, cb);
   }
 }
 
