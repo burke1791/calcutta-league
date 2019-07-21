801df55844aafd5bd56b29f6dd12189086af422c
Chris Burke <burke1791@gmail.com>
2019-07-21 14:13:56 -0500

    added a function that packages the user's info in a nice object for the front end

--- models/user.js
+++ models/user.js
@@ -6,11 +6,25 @@ let user = {
       table: 'users',
       data: userObj // keys must match column names
     };
-    orm.insert(queryParams, cb);
+    orm.insert(queryParams, (err, result) => {
+      console.log(result);
+      let userInfo = {
+        user_id: result.insertId,
+        alias: userObj.alias,
+        leagues: []
+      };
+
+      if (err) {
+        console.log(err);
+        cb(err, null);
+      } else {
+        cb(null, userInfo);
+      }
+    });
   },
   
   selectUserInfo: (uid, cb) => {
-    let queryString = 'SELECT ?? FROM users JOIN league_membership ON users.user_id = league_membership.user_id JOIN leagues ON league_membership.league_id = leagues.league_id JOIN auctions ON leagues.league_id = auctions.league_id WHERE ?';
+    let queryString = 'SELECT ?? FROM users LEFT JOIN league_membership ON users.user_id = league_membership.user_id LEFT JOIN leagues ON league_membership.league_id = leagues.league_id LEFT JOIN auctions ON leagues.league_id = auctions.league_id WHERE ?';
     
     let queryArray = [
       'users.user_id',
@@ -27,8 +41,55 @@ let user = {
     };
 
     // orm.query is for testing new queries - will refactor to be more orm-y
-    orm.query(queryString, queryArray, where, cb);
+    orm.query(queryString, queryArray, where, (err, result) => {
+      let userInfo = packageUserInfoAndCheckIntegrity(result);
+
+      if (err) {
+        console.log(err);
+        cb(err, null);
+      } else {
+        cb(null, userInfo);
+      }
+    });
   }
+}
+
+// this function checks the integrity of the user's data coming back from MySQL
+// at the moment, it only checks if the user_id and alias properties all match
+const packageUserInfoAndCheckIntegrity = (data) => {
+  let user_id = data[0].user_id;
+  let alias = data[0].alias;
+
+  let packagedUserInfo = {
+    user_id: '',
+    alias: '',
+    leagues: []
+  };
+  
+  // this loop both does an integrity check and packages the user info for the front end
+  for (var row of data) {
+    // checks to make sure the user_id and alias properties are the same for every array item
+    if (row.user_id !== user_id || row.alias !== alias) {
+      return false;
+    }
+
+    // will end up being set to the last user_id and alias entries, but they should all be the same anyways
+    // will optimize in the future if necessary
+    packagedUserInfo.user_id = row.user_id;
+    packagedUserInfo.alias = row.alias;
+    if (row.league_id) {
+      packagedUserInfo.leagues.push({
+        league_id: row.league_id,
+        league_name: row.league_name,
+        league_status: row.league_status,
+        role: row.role,
+        auction_id: row.auction_id
+      });
+    }
+    
+  }
+  
+  return packagedUserInfo;
 }
 
 module.exports = user;
