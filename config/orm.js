f3892b11bf7df0941ed401ac940a82e9521ab4fb
Chris Burke <burke1791@gmail.com>
2019-07-24 11:42:40 -0500

    added a giant mess of helper functions that will be useful in the future, but I got tired of trying to get the league summary query to work programatically so I hard coded it

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 let orm = {
   selectWhere: (params, cb) => {
-    let queryString = 'SELECT ?? FROM ?? WHERE' + queryWhereBuilder(params.where, 'AND');
+    let queryString = 'SELECT ?? FROM ?? WHERE' + queryWhereBuilderTest(params.where, 'AND');
     let queryParams = [params.select, params.from].concat(params.where.values);
     
     let query = connection.query(queryString, queryParams, (err, result) => {
@@ -32,6 +32,38 @@ let orm = {
     console.log(query.sql);
   },
 
+  // spent too many hours trying to programatically get this query to work
+  // just going to hard code it until I'm not as much of a moron
+  selectJoinWhereGroup: (uid, cb) => {
+    // let queryString = 'SELECT ??';
+    // queryString += querySumBuilder(params.sum);
+    // queryString += 'FROM ??';
+    // queryString += queryJoinBuilder(params.joinTable, 'LEFT');
+    // queryString += queryWhereBuilder(params.where, 'AND');
+    // queryString += queryGroupBuilder(params.group);
+
+    // let options = [params.columns, params.table];
+    // options.push(...params.joinCondition);
+    // options.push(...params.where);
+    // options.push(params.group);
+
+    let queryString = `SELECT u.user_id, l.league_id, l.league_name, l.league_status, l.auction_id, lm.role, SUM(lt.price) AS buyIn, SUM(lt.return) AS payout
+    FROM league_membership lm
+    LEFT JOIN users u
+    ON ?
+    LEFT JOIN league_teams lt
+    ON lt.user_id = u.user_id AND lt.league_id = lm.league_id
+    LEFT JOIN leagues l
+    ON l.league_id = lm.league_id
+    WHERE lm.league_id = l.league_id AND lm.user_id = u.user_id
+    GROUP BY l.league_id, u.user_id`;
+
+    let query = connection.query(queryString, uid, (err, result) => {
+      cb(err, result);
+    });
+    console.log(query.sql);
+  },
+
   // for testing new queries from the models
   query: (queryString, queryArray, where, cb) => {
     let query = connection.query(queryString, [queryArray, where], (err, result) => {
@@ -41,7 +73,7 @@ let orm = {
   }
 }
 
-const queryWhereBuilder = (where, logical) => {
+const queryWhereBuilderTest = (where, logical) => {
   let whereString = ' ';
 
   for (var i in where.columns) {
@@ -52,6 +84,55 @@ const queryWhereBuilder = (where, logical) => {
   }
 
   return whereString;
+}
+
+const queryWhereBuilder = (where, logical) => {
+  let whereString = ' ';
+
+  for (var i in where) {
+    if (i > 0) {
+      whereString += ' ' + logical + ' ?';
+    } else {
+      whereString += 'WHERE ?';
+    }
+  }
+
+  return whereString;
+}
+
+const queryJoinBuilder = (joinTable, direction) => {
+  let joinString = ' ';
+
+  for (var i in joinTable) {
+    // if someone tries to insert a drop statement, void this join builder
+    if (joinTable[i].toLowerCase().includes('drop') || joinTable[i].includes(';')) {
+      return null;
+    } else {
+      joinString += ' ' + direction + ' JOIN ';
+      joinString += joinTable[i];
+      joinString += ' ON ?';
+    }
+  }
+  return joinString;
+}
+
+const queryGroupBuilder = (groupBy) => {
+  let groupString = ' GROUP BY ??';
+  return groupString;
+}
+
+const querySumBuilder = (sum) => {
+  let sumString = ' ';
+  for (var i in sum) {
+    // if someone tries to insert a drop statement, void this join builder
+    if (sum[i].toLowerCase().includes('drop') || sum[i].includes(';')) {
+      return null;
+    } else {
+      sumString += ', SUM(' + sum[i] + ') ';
+    }
+  }
+
+  return sumString;
 }
 
 module.exports = orm;
