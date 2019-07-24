621e56c7020a878c5723d92db493d603cd39c095
Chris Burke <burke1791@gmail.com>
2019-07-24 11:40:15 -0500

    refactored the logic to fetch league summary info on sign in as well as on league join

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -8,7 +8,8 @@ import LeagueModal from '../leagueModal/leagueModal';
 import { NOTIF, LEAGUE_FORM_TYPE } from '../../utilities/constants';
 import Pubsub from '../../utilities/pubsub';
 import { User } from '../../firebase/authService';
-import { Data } from '../../utilities/data';
+import DataService, { Data } from '../../utilities/data';
+import { formatMoney } from '../../utilities/helper';
 
 function Main() {
 
@@ -17,12 +18,24 @@ function Main() {
   useEffect(() => {
     Pubsub.subscribe(NOTIF.SIGN_IN, this, handleSignin);
     Pubsub.subscribe(NOTIF.SIGN_OUT, this, handleSignout);
+    Pubsub.subscribe(NOTIF.LEAGUE_JOINED, this, handleLeagueJoin);
+    Pubsub.subscribe(NOTIF.LEAGUE_SUMMARIES_FETCHED, this, handleNewLeagueInfo);
 
     return (() => {
       Pubsub.unsubscribe(NOTIF.SIGN_IN, this);
       Pubsub.unsubscribe(NOTIF.SIGN_OUT, this);
+      Pubsub.unsubscribe(NOTIF.LEAGUE_JOINED, this);
+      Pubsub.unsubscribe(NOTIF.LEAGUE_SUMMARIES_FETCHED, this);
     });
   }, []);
+
+  const fetchLeagueInfo = () => {
+    DataService.updateLeagueInfo();
+  }
+
+  const handleLeagueJoin = () => {
+    fetchLeagueInfo();
+  }
 
   // copy the summary info from Data into local state, which triggers a rerender
   const handleNewLeagueInfo = () => {
@@ -31,9 +44,9 @@ function Main() {
         return Data.leagues.map(league => {
           return {
             name: league.league_name,
-            buyIn: league.buy_in || '$0.00',
-            payout: league.payout || '$0.00',
-            return: league.return || '$0.00',
+            buyIn: formatMoney(league.buyIn || '0'),
+            payout: formatMoney(league.payout || '0'),
+            return: formatMoney(league.buyIn && league.payout ? league.payout - league.buyIn : '0'),
             key: league.league_id
           };
         });
@@ -44,7 +57,7 @@ function Main() {
   // loads in league summary info from global state
   // and rerenders the table on the main page
   const handleSignin = () => {
-    handleNewLeagueInfo();
+    fetchLeagueInfo();
   }
 
   // empties league summaries info on signout,
