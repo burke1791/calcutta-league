54e3b47cba15de6fcc74933044eedbedd42f134b
Chris Burke <burke1791@gmail.com>
2019-07-24 02:07:58 -0500

    added logic to load and unload the league summary info on signin/signout

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col, Button } from 'antd';
 import 'antd/dist/antd.css';
 
@@ -8,8 +8,50 @@ import LeagueModal from '../leagueModal/leagueModal';
 import { NOTIF, LEAGUE_FORM_TYPE } from '../../utilities/constants';
 import Pubsub from '../../utilities/pubsub';
 import { User } from '../../firebase/authService';
+import { Data } from '../../utilities/data';
 
 function Main() {
+
+  const [leagueSummaries, setLeagueSummaries] = useState([]);
+
+  useEffect(() => {
+    Pubsub.subscribe(NOTIF.SIGN_IN, this, handleSignin);
+    Pubsub.subscribe(NOTIF.SIGN_OUT, this, handleSignout);
+
+    return (() => {
+      Pubsub.unsubscribe(NOTIF.SIGN_IN, this);
+      Pubsub.unsubscribe(NOTIF.SIGN_OUT, this);
+    });
+  }, []);
+
+  // copy the summary info from Data into local state, which triggers a rerender
+  const handleNewLeagueInfo = () => {
+    setLeagueSummaries(
+      (() => {
+        return Data.leagues.map(league => {
+          return {
+            name: league.league_name,
+            buyIn: league.buy_in || '$0.00',
+            payout: league.payout || '$0.00',
+            return: league.return || '$0.00',
+            key: league.league_id
+          };
+        });
+      })()
+    );
+  }
+
+  // loads in league summary info from global state
+  // and rerenders the table on the main page
+  const handleSignin = () => {
+    handleNewLeagueInfo();
+  }
+
+  // empties league summaries info on signout,
+  // which triggers a rerender to show the default table on the main page
+  const handleSignout = () => {
+    setLeagueSummaries([]);
+  }
 
   const newLeague = () => {
     console.log('new league clicked');
@@ -36,7 +78,7 @@ function Main() {
         <Button type='primary' onClick={joinLeague} style={{ margin: '20px 12px' }}>Join a League</Button>
       </Row>
       <Row type='flex' justify='center'>
-        <LeagueTable type='in-progress' list={[]} />
+        <LeagueTable type='in-progress' list={leagueSummaries} />
       </Row>
       <LeagueModal />
     </div>
