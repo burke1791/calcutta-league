3e42cd6a978c6d0dfce5dad03286b66d345a6ea9
Chris Burke <burke1791@gmail.com>
2019-07-25 01:12:19 -0500

    created the navbar for the league component

--- client/src/components/leagueNav/leagueNav.js
+++ client/src/components/leagueNav/leagueNav.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Menu } from 'antd';
+import 'antd/dist/antd.css';
+import { navigate } from '@reach/router';
 
 function LeagueNav(props) {
+
+  const handleLeagueNavClick = (event) => {
+    if (event.key == 'leagueHome') {
+      navigate(`/leagues/${props.leagueId}`);
+    } else if (event.key == 'auction') {
+      navigate(`/leagues/${props.leagueId}/auction`);
+    }
+  }
+
   return (
-    <div>
-      League Nav
-    </div>
+    <nav className='leaguenav'>
+      <Menu mode='horizontal' onClick={handleLeagueNavClick} style={{ lineHeight: '48px' }}>
+        <Menu.Item key='leagueHome'>
+          League Home
+        </Menu.Item>
+        <Menu.Item key='auction'>
+          Auction
+        </Menu.Item>
+      </Menu>
+    </nav>
   );
 }
 
