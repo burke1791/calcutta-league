32d8a51d811b711d10b370bde9aa857ce579d214
Chris Burke <burke1791@gmail.com>
2019-07-25 00:32:39 -0500

    created default structure with router wrapper

--- client/src/components/league/league.js
+++ client/src/components/league/league.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Router } from '@reach/router';
+
+import LeagueNav from '../leagueNav/leagueNav';
+import LeagueHome from '../leagueHome/leagueHome';
+import LeagueAuction from '../leagueAuction/leagueAuction';
 
 function League(props) {
 
   return (
     <div>
-      <h1>LeagueId: {props.leagueId}</h1>
+      <LeagueNav />
+      <Router>
+        <LeagueHome path='/' />
+        <LeagueAuction path='/auction/' />
+      </Router>
     </div>
   );
 }
