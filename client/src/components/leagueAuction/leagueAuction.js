153ba34335ed9f753a690e2aafb3db49a9b16615
Chris Burke <burke1791@gmail.com>
2019-07-28 12:46:16 -0500

    added test data in state and effect hooks and basic layout of the page

--- client/src/components/leagueAuction/leagueAuction.js
+++ client/src/components/leagueAuction/leagueAuction.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AuctionTeams from '../auctionTeams/auctionTeams';
+import AuctionActions from '../auctionActions/auctionActions';
+
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 
 function LeagueAuction(props) {
+
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    // API call to fetch teams
+    
+    // TEST DATA
+    setTeams([
+      {
+        name: 'Illinois',
+        price: null,
+        owner: null
+      },
+      {
+        name: 'Ohio State',
+        price: 12,
+        owner: 'burkcules'
+      }
+    ]);
+  }, [])
+  // AuctionTeams
+    // list of teams: name, price, purchaser, logo url
+  // AuctionActions
+    // firebase auctionId
+  // Chat
+    // firebase auctionId
+  // MyTeams
+    // list of teams I purchased: name, price
+  // MemberList
+    // list of users in this league with total spent
+
   return (
-    <div>
-      League Auction
-    </div>
+    <Row>
+      <Col span={8}>
+        <AuctionTeams teams={teams} />
+      </Col>
+      <Col span={8}>
+        <AuctionActions auctionId={props.auctionId} />
+        {/* Chat */}
+      </Col>
+      <Col span={8}>
+        {/* My Teams List */}
+        {/* Member List */}
+      </Col>
+    </Row>
   );
 }
 
