0e06c906fc45d945d00b5739914a76c0e6656866
Chris Burke <burke1791@gmail.com>
2019-07-29 00:23:02 -0500

    imported firestore

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import firebase_config from './firebase.config.json';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const env = process.env.REACT_APP_ENV || 'development';
 
@@ -9,7 +10,11 @@ const config = firebase_config[env];
 firebase.initializeApp(config);
 
 const auth = firebase.auth();
+const db = firebase.firestore();
+const dbObj = firebase.firestore;
 
 export {
-  auth
+  auth,
+  db,
+  dbObj
 };
