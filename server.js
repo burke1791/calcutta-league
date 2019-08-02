ddea3661a4c6a6f27b07197b7dcc82bbd26415c9
Chris Burke <burke1791@gmail.com>
2019-08-02 14:34:51 -0500

    required the auctions controller

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 require('./controllers/users')(app);
 require('./controllers/leagues')(app);
+require('./controllers/auctions')(app);
 
 app.listen(PORT, () => {
   console.log('listening on port: ' + PORT);
