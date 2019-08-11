61f28c1356389de8e01119faf9dccbd2e07ff5ae
Chris Burke <burke1791@gmail.com>
2019-08-11 18:52:03 -0500

    added auction_id to the league user summaries query

--- models/league.js
+++ models/league.js
@@ -79,7 +79,7 @@ let league = {
   selectLeagueUserSummaries: (leagueObj, cb) => {
     let queryParams = {
       table: 'league_membership',
-      columns: ['users.user_id', 'users.alias', 'leagues.league_name'],
+      columns: ['users.user_id', 'users.alias', 'leagues.league_name', 'leagues.auction_id'],
       sum: ['league_teams.price', 'league_teams.return'],
       sumAlias: ['buyIn', 'payout'],
       joinTable: ['users', 'leagues', 'league_teams'],
