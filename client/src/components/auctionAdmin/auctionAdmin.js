00b64559eccf7bc6a8a64727fa521c0089923731
Chris Burke <burke1791@gmail.com>
2019-08-11 03:25:12 -0500

    refactored logic for displaying the start/stop button

--- client/src/components/auctionAdmin/auctionAdmin.js
+++ client/src/components/auctionAdmin/auctionAdmin.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Button } from 'antd';
 import 'antd/dist/antd.css';
+import { AUCTION_STATUS } from '../../utilities/constants';
 
 const btnStyle = {
   marginTop: '4px'
@@ -15,14 +16,33 @@ const containerStyle = {
 function AuctionAdmin(props) {
   
   const generateStartStopButton = () => {
-    if (props.inProgress) {
-      return (
-        <Button type='danger' style={btnStyle}>Stop Auction</Button>
-      );
-    } else {
-      return (
-        <Button type='primary' style={btnStyle}>Start Auction</Button>
-      );
+    let btnText = 'Start Auction';
+    let btnType = 'primary';
+    let disabled = false;
+    let name = 'start';
+
+    if (props.status === AUCTION_STATUS.IN_PROGRESS || props.status === AUCTION_STATUS.ITEM_COMPLETE) {
+      btnText = 'Stop Auction';
+      btnType = 'danger';
+      name = 'stop'
+    } else if (props.status === AUCTION_STATUS.END) {
+      btnText = 'Auction Complete';
+      btnType = 'primary'
+      disabled = true;
+      name = 'n/a';
+    }
+
+    return <Button type={btnType} disabled={disabled} style={btnStyle} onClick={startStopClick}>{btnText}</Button>
+  }
+
+  const startStopClick = (event) => {
+    event.preventDefault();
+    let name = event.target.name;
+
+    if (name == 'start') {
+      // Start auction
+    } else if (name == 'stop') {
+      // Stop auction
     }
   }
   
