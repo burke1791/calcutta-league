0b1ffb38d887d565886fcd91dd8c861a7d7ea703
Chris Burke <burke1791@gmail.com>
2019-08-31 07:24:53 -0500

    added call to the message model to fetch an array of topics for a given league

--- controllers/messages.js
+++ controllers/messages.js
@@ -6,10 +6,15 @@ module.exports = function(app) {
   // checks if the user is a member of the league by its leagueId, then returns all of the topics in that league's message board
   app.get('/api/message_board/all/:leagueId', (req, res, next) => {
     let params = {
-      topic_id: req.params.leagueId
+      league_id: req.params.leagueId
     };
-    // check if user is allowed to access the message board of this league
-    res.end();
+    message.getTopicsByLeagueId(params, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).json(result);
+      }
+    });
   });
 
   // checks if the user is a member of the league by its id (deduced via the topic id), then returns all of the messages in that topic's thread
