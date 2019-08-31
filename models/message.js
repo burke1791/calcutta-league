3a549ab162030eda764e97c27994c4f252f2b675
Chris Burke <burke1791@gmail.com>
2019-08-31 07:25:14 -0500

    added a function to query for an array of topics for a given league

--- models/message.js
+++ models/message.js
@@ -1,7 +1,16 @@
 const orm = require('../config/orm');
 
 let message = {
-
+  getTopicsByLeagueId: (messageObj, cb) => {
+    let queryParams = {
+      table: 'message_board',
+      columns: ['message_board.topic_id', 'message_board.league_id', 'message_board.user_id', 'message_board.title', 'message_board.created'],
+      where: [
+        { 'message_board.league_id': messageObj.league_id }
+      ]
+    };
+    orm.select(queryParams, cb);
+  }
 };
 
-export default message;
+module.exports = message;
