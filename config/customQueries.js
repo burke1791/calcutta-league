d429ab776471571abfb3177dfd3b0527ad0add1e
Chris Burke <burke1791@gmail.com>
2019-11-12 16:34:28 -0600

    created a file to contain all of the complex queries the orm is not yet capable of handling

---
+++ config/customQueries.js
@@ -0,0 +1,15 @@
+// potentially a temporary file until I expand the orm's capabilities
+
+const connection = require('./connection');
+
+let customQueries = {
+  getMessageThreadMetaData: (leagueId, cb) => {
+    let queryString = 'Select mb.title, mb.topic_id As topicId, u.alias As threadAuthor, mb.created As threadCreated, lp.alias As lastPostAuthor, lp.created As lastPostCreated, Count(mt.message_id) As postCount From message_board mb Inner Join message_thread mt On mb.topic_id = mt.topic_id Inner Join users u On mb.user_id = u.user_id Inner Join ( Select u.alias, mt.topic_id, Max(mt.created) As created From message_thread mt Inner Join users u On mt.user_id = u.user_id Group By u.alias, mt.topic_id ) lp On mb.topic_id = lp.topic_id Where mb.league_id = ' + leagueId + ' Group By mb.title, mb.topic_id, u.alias, mb.created, lp.alias, lp.created';
+
+    let query = connection.query(queryString, (err, result) => {
+      cb(err, result);
+    });
+  }
+}
+
+module.exports = customQueries;
