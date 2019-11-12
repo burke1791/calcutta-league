b802994b988f1c0a4e2c1983f37471d737fc4b4f
Chris Burke <burke1791@gmail.com>
2019-11-12 16:34:05 -0600

    changed standard orm logic into a call to the customQueries object

--- models/message.js
+++ models/message.js
@@ -1,15 +1,18 @@
 const orm = require('../config/orm');
+const customQueries = require('../config/customQueries');
 
 let message = {
   getTopicsByLeagueId: (messageObj, cb) => {
-    let queryParams = {
-      table: 'message_board',
-      columns: ['message_board.topic_id', 'message_board.league_id', 'message_board.user_id', 'message_board.title', 'message_board.created'],
-      where: [
-        { 'message_board.league_id': messageObj.league_id }
-      ]
-    };
-    orm.select(queryParams, cb);
+    // let queryParams = {
+    //   table: 'message_board',
+    //   columns: ['message_board.topic_id', 'message_board.league_id', 'message_board.user_id', 'message_board.title', 'message_board.created'],
+    //   where: [
+    //     { 'message_board.league_id': messageObj.league_id }
+    //   ]
+    // };
+    // orm.select(queryParams, cb);
+
+    customQueries.getMessageThreadMetaData(messageObj.league_id, cb);
   }
 };
 
