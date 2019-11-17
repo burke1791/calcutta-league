40f006ba17727b4f5f8540d482feaeb58a2c068c
Chris Burke <burke1791@gmail.com>
2019-11-17 14:08:30 -0600

    created a component for the new topic form

---
+++ client/src/components/newTopicForm/newTopicForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
+import DataService from '../../utilities/data';
+
+const { TextArea } = Input;
+
+function NewTopicForm(props) {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { getFieldDecorator } = props.form;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        props.toggleLoading();
+
+        let title = values.topicTitle;
+        let content = values.topicContent;
+
+        console.log('title: ' + title);
+        console.log('content: ' + content);
+
+        // make a post request
+        DataService.postNewMessageBoardTopic(props.leagueId, title, content).catch(error => {
+          // @TODO refactor to proper error handling
+          setErrorMessage('Server Error, please try again later');
+          props.toggleLoading(false);
+        });
+      } else {
+        alert('Validation Error');
+      }
+    });
+  }
+
+  const generateErrorMessage = () => {
+    if (errorMessage) {
+      return (
+        <span className='ant-form-text' style={{ color: 'cf1322' }}>{errorMessage}</span>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  return (
+    <Form onSubmit={handleSubmit} className='new-topic-form' style={{ maxWidth: '300px' }}>
+      <Form.Item>
+        {getFieldDecorator('topicTitle', {
+          rules: [
+            {
+              required: true,
+              message: 'Please enter a title!'
+            }
+          ]
+        })(
+          <Input
+            type='text'
+            placeholder='Title'
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator('topicContent', {
+          rules: [
+            {
+              required: true,
+              message: 'Don\'t be shy, speak your mind!'
+            }
+          ]
+        })(
+          <TextArea rows={4} />
+        )}
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' loading={props.loading} htmlType='submit'>Submit</Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+const WrappedNewTopicForm = Form.create({ name: 'new_topic_form' })(NewTopicForm);
+
+export default WrappedNewTopicForm;
