fb8423e1774dbeb944e5966757032a9f6ae27cac
Chris Burke <burke1791@gmail.com>
2019-12-14 11:14:36 -0600

    adjusted the padding on the topnav header

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Main from './components/main/main';
 import { Layout } from 'antd';
 import League from './components/league/league';
 import LandingPage from './landingPage/landingPage';
+import Admin from './components/adminPage/admin';
 
 const { Header, Footer, Content } = Layout;
 
@@ -16,12 +17,13 @@ function App() {
   return (
     <div className="App">
       <Layout style={{ height: '100vh' }}>
-        <Header>
+        <Header style={{ padding: '0 25px' }}>
           <Topnav />
         </Header>
         <Content style={{ margin: '0' }}>
           <Router>
             <LandingPage path='/' />
+            <Admin path='/admin' />
             <Main path='/home' />
             <League path='leagues/:leagueId/*' />
           </Router>
