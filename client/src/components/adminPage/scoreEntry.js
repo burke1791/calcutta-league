41e2d7ecd5b3523d01f9c900fcd30bdcdd794f7d
Chris Burke <burke1791@gmail.com>
2019-12-17 00:28:56 -0600

    refactored handling of results to match key values

--- client/src/components/adminPage/scoreEntry.js
+++ client/src/components/adminPage/scoreEntry.js
@@ -5,6 +5,7 @@ import 'antd/dist/antd.css';
 
 import Pubsub from '../../utilities/pubsub';
 import { NOTIF } from '../../utilities/constants';
+import AdminService from '../../utilities/adminService';
 
 function ScoreEntry(props) {
 
@@ -13,15 +14,27 @@ function ScoreEntry(props) {
   const [loading, setLoading] = useState({});
 
   useEffect(() => {
-    Pubsub.subscribe(NOTIF.MM_RESULTS_DOWNLOADED, ScoreEntry, handleResults);
+    handleMarchMadnessResults();
+    Pubsub.subscribe(NOTIF.MM_RESULTS_DOWNLOADED, ScoreEntry, handleMarchMadnessResults);
 
     return (() => {
       Pubsub.unsubscribe(NOTIF.MM_RESULTS_DOWNLOADED, ScoreEntry);
     });
   }, []);
 
-  const handleResults = () => {
-    // setTeams
+  const handleMarchMadnessResults = () => {
+    console.log('handling results');
+    setTeams(AdminService.marchMadnessResults);
+
+    if (AdminService.marchMadnessResults && AdminService.marchMadnessResults.length) {
+      let loadingObj = {};
+      for (let game of AdminService.marchMadnessResults) {
+        let gameCode = 'R' + game.round + game.gameId;
+        loadingObj[gameCode] = false;
+      }
+
+      setLoading(loadingObj);
+    }
   }
 
   const teamLabel = (teamObj) => {
@@ -31,42 +44,47 @@ function ScoreEntry(props) {
   const getFields = () => {
     const { getFieldDecorator } = props.form;
     const children = [];
-    for (let gameCode in teams) {
-      children.push(
-        <Row key={gameCode} type='flex' style={{ justifyContent: 'space-between' }}>
-          <Col span={10}>
-            <Form.Item label={teamLabel(teams[gameCode].team1)} style={{ display: 'flex' }}>
-              {getFieldDecorator(`${gameCode}_team1`, {
-                rules: [
-                  {
-                    type: 'number',
-                    message: 'Must be a number',
-                  },
-                ],
-                initialValue: teams[gameCode].team1.score
-              })(<InputNumber size='small' min={0} max={999} />)}
-            </Form.Item>
-          </Col>
-          <Col span={10}>
-            <Form.Item label={teamLabel(teams[gameCode].team2)} style={{ display: 'flex' }}>
-              {getFieldDecorator(`${gameCode}_team2`, {
-                rules: [
-                  {
-                    type: 'number',
-                    message: 'Must be a number',
-                  },
-                ],
-                initialValue: teams[gameCode].team2.score
-              })(<InputNumber size='small' min={0} max={999} />)}
-            </Form.Item>
-          </Col>
-          <Col span={4}>
-            <Button type='default' icon='check' name={gameCode} onClick={handleSubmit} loading={loading[gameCode]} />
-          </Col>
-        </Row>
-      );
+    if (teams && teams.length) {
+      for (let game of teams) {
+        let gameCode = 'R' + game.round + game.gameId
+        children.push(
+          <Row key={gameCode} type='flex' style={{ justifyContent: 'space-between' }}>
+            <Col span={10}>
+              <Form.Item label={teamLabel(game.team1)} style={{ display: 'flex' }}>
+                {getFieldDecorator(`${gameCode}_team1`, {
+                  rules: [
+                    {
+                      type: 'number',
+                      message: 'Must be a number',
+                    },
+                  ],
+                  initialValue: game.team1.score
+                })(<InputNumber size='small' min={0} max={999} />)}
+              </Form.Item>
+            </Col>
+            <Col span={10}>
+              <Form.Item label={teamLabel(game.team2)} style={{ display: 'flex' }}>
+                {getFieldDecorator(`${gameCode}_team2`, {
+                  rules: [
+                    {
+                      type: 'number',
+                      message: 'Must be a number',
+                    },
+                  ],
+                  initialValue: game.team2.score
+                })(<InputNumber size='small' min={0} max={999} />)}
+              </Form.Item>
+            </Col>
+            <Col span={4}>
+              <Button type='default' icon='check' name={gameCode} onClick={handleSubmit} loading={loading[gameCode]} />
+            </Col>
+          </Row>
+        );
+      }
+      return children;
+    } else {
+      return null;
     }
-    return children;
   }
 
   const handleSubmit = (e) => {
